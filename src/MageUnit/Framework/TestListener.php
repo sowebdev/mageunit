f9afb77716158138b5fb78a29e7a7c4e076d63e8
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2014-06-29 10:05:05 +0200

    Ensure initialization runs only once in a test suite

--- src/MageUnit/Framework/TestListener.php
+++ src/MageUnit/Framework/TestListener.php
@@ -2,20 +2,30 @@
 class MageUnit_Framework_TestListener extends PHPUnit_Framework_BaseTestListener
 {
     /**
+     * @var PHPUnit_Framework_TestSuite|null
+     */
+    protected $_testSuiteRoot;
+
+    /**
      * Initialize test environment
      *
      * @param PHPUnit_Framework_TestSuite $suite
      */
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
-        $reflectionClass = new ReflectionClass('Mage');
-        $reflectionProperty = $reflectionClass->getProperty('_app');
-        $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue(new MageUnit_Mock_Core_Model_App());
-        /**
-         * @var $app MageUnit_Mock_Core_Model_App
-         */
-        $app = $reflectionProperty->getValue();
-        $app->initTestEnvironment();
+        //Ensures initialization runs only once in a test suite
+        if ($this->_testSuiteRoot === null) {
+            $reflectionClass = new ReflectionClass('Mage');
+            $reflectionProperty = $reflectionClass->getProperty('_app');
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue(new MageUnit_Mock_Core_Model_App());
+            /**
+             * @var $app MageUnit_Mock_Core_Model_App
+             */
+            $app = $reflectionProperty->getValue();
+            $app->initTestEnvironment();
+
+            $this->_testSuiteRoot = $suite;
+        }
     }
 }
