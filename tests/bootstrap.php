7d2a378e8be3b2abb7ddec070e09b64ad3b584d1
Sören Ohnmeiss <soren.ohnmeiss@netapsys.fr>
2014-12-24 14:56:53 +0100

    Use Exception instead of Mage_Core_Exception in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 if (!getenv('MAGE')) {
-    throw new Mage_Core_Exception('Path to Mage.php has not been configured');
+    throw new Exception('Path to Mage.php has not been configured');
 }
 require getenv('MAGE');
 
