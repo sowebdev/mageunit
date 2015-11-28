8881f4c49028d081c3a7a40a07ea7bc426268e00
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2015-11-28 23:14:39 +0100

    Fixed store mock for old Magento versions (1.4/1.5) - don't extend class which doesn't exist

--- src/MageUnit/Mock/Core/Model/Mysql4/Store/Collection.php
+++ src/MageUnit/Mock/Core/Model/Mysql4/Store/Collection.php
@@ -1,4 +1,13 @@
 <?php
-class MageUnit_Mock_Core_Model_Mysql4_Store_Collection extends MageUnit_Mock_Core_Model_Resource_Store_Collection
+class MageUnit_Mock_Core_Model_Mysql4_Store_Collection extends Mage_Core_Model_Mysql4_Store_Collection
 {
+    /**
+     *  Define resource model
+     */
+    protected function _construct()
+    {
+        $this->setFlag('load_default_store', false);
+        $this->_init('core/store');
+        $this->_itemObjectClass = 'MageUnit_Mock_Core_Model_Store';
+    }
 }
