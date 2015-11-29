acc1bf0707fe4b450eae1c770adfd613eb9efbc1
Sören Ohnmeiss <soren.ohnmeiss@gmail.com>
2015-11-29 16:02:42 +0100

    Deleted tests which were close to be duplicates

--- tests/MageUnit/ConfigTest.php
+++ tests/MageUnit/ConfigTest.php
@@ -10,32 +10,4 @@ class MageUnit_ConfigTest extends PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf('MageUnit_Mock_Core_Model_Config', Mage::app()->getConfig());
     }
-
-    public function testOriginalGetModelInstance()
-    {
-        $modelInstance = Mage::app()->getConfig()->getModelInstance('core/store');
-        $this->assertInstanceOf('Mage_Core_Model_Store', $modelInstance);
-    }
-
-    public function testMockedModelInstance()
-    {
-        Mage::app()->getConfig()->registerModelMock('core/store', new stdClass());
-        $modelInstance = Mage::app()->getConfig()->getModelInstance('core/store');
-        $this->assertInstanceOf('stdClass', $modelInstance);
-    }
-
-    /**
-     * Model factories should be able to return either new instances or singletons
-     *
-     * @ticket #3
-     */
-    public function testGetModelCanReturnNewInstanceOnEachCall()
-    {
-        Mage::app()->getConfig()->registerModelMock('core/store', 'stdClass');
-        $modelInstance = Mage::app()->getConfig()->getModelInstance('core/store');
-        $this->assertInstanceOf('stdClass', $modelInstance);
-        $modelInstance->id = 'something';
-        $anotherModelInstance = Mage::app()->getConfig()->getModelInstance('core/store');
-        $this->assertObjectNotHasAttribute('id', $anotherModelInstance);
-    }
 }
--- tests/MageUnit/LayoutTest.php
+++ tests/MageUnit/LayoutTest.php
@@ -10,17 +10,4 @@ class MageUnit_LayoutTest extends PHPUnit_Framework_TestCase
     {
         $this->assertInstanceOf('MageUnit_Mock_Core_Model_Layout', Mage::app()->getLayout());
     }
-
-    public function testOriginalGetBlockInstance()
-    {
-        $blockInstance = Mage::app()->getLayout()->createBlock('core/template');
-        $this->assertInstanceOf('Mage_Core_Block_Template', $blockInstance);
-    }
-
-    public function testMockedBlockInstance()
-    {
-        Mage::app()->getLayout()->registerBlockMock('core/template', new Varien_Object());;
-        $blockInstance = Mage::app()->getLayout()->createBlock('core/template');
-        $this->assertInstanceOf('Varien_Object', $blockInstance);
-    }
 }
